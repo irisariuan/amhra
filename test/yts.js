8723516c234ebf5cc09e1c998df488f064f0623e
ariuan <kms12836@gmail.com>
2023-12-28 13:35:42 +0800

    remove test files

--- test/yts.js
+++
@@ -1,4 +0,0 @@
-const yts = require('yt-search')
-
-yts('https://www.youtube.com/playlist?list=PLUPOh9x7B9R5Py6r7K58Akxep6amb43OA').then(v => console.log(v))
-yts({listId: 'PLUPOh9x7B9R5Py6r7K58Akxep6amb43OA'}).then(v => console.log(v))
