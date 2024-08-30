2febbcb8745bb576de4adda4e139a36af0f31673
ariuan <kms12836@gmail.com>
2024-08-30 20:04:05 +0800

    added flag startup

--- index.ts
+++ index.ts
@@ -8,24 +8,30 @@ import { exp, globalApp } from "./lib/misc"
 const setting = readJsonSync()
 
 	; (async () => {
-		const choices: { name: string, value: 'prod' | 'dev' }[] = []
-		if (setting.TOKEN) {
-			process.on("uncaughtException", e => {
-				globalApp.err(`Uncaught Error: ${e}`)
-			})
-
-			process.on("unhandledRejection", error => {
-				globalApp.err("Unhandled promise rejection:", error)
-			})
-			choices.push({ name: "Production", value: "prod" })
+		let result: 'prod' | 'dev'
+		if (process.argv.includes("--prod") && setting.TOKEN) {
+			console.log(chalk.bgGreen.whiteBright("Flagged Production Mode"))
+			result = 'prod'
+		} else {
+			const choices: { name: string, value: 'prod' | 'dev' }[] = []
+			if (setting.TOKEN) {
+				process.on("uncaughtException", e => {
+					globalApp.err(`Uncaught Error: ${e}`)
+				})
+	
+				process.on("unhandledRejection", error => {
+					globalApp.err("Unhandled promise rejection:", error)
+				})
+				choices.push({ name: "Production", value: "prod" })
+			}
+			if (setting.TESTING_TOKEN) {
+				choices.push({ name: "Development", value: "dev" })
+			}
+			if (choices.length === 0) {
+				return console.log(chalk.bgRed.whiteBright("No token is provided"))
+			}
+			result = await select({ choices: choices, message: "Mode" })
 		}
-		if (setting.TESTING_TOKEN) {
-			choices.push({ name: "Development", value: "dev" })
-		}
-		if (choices.length === 0) {
-			return console.log(chalk.bgRed.whiteBright("No token is provided"))
-		}
-		const result = await select({ choices: choices, message: "Mode" })
 
 		const token = { prod: setting.TOKEN, dev: setting.TESTING_TOKEN }[result]
 		const app = await init(client)
