f4a2c12d1f65cf9d89a4340877b1529672ae3023
ariuan <kms12836@gmail.com>
2024-11-06 00:05:41 +0800

    refactor record command to simplify recording logic and improve response messages

--- commands/slash/record.ts
+++ commands/slash/record.ts
@@ -2,22 +2,18 @@ import type { Command } from "../../lib/interaction"
 import { SlashCommandBuilder } from 'discord.js'
 import { saveRecord, startRecord } from "../../lib/voice/record"
 
-const record: string[] = []
-
 export default {
 	data: new SlashCommandBuilder()
 		.setName('record')
 		.setDescription('Record your conversation'),
 	async execute(interaction, client) {
 		if (!interaction.guildId) return
-		if (record.includes(interaction.guildId)) {
-			await saveRecord(interaction, 20)
-			await startRecord(interaction)
-			return interaction.reply({ content: 'Saving!', ephemeral: true })
+		if (!startRecord(interaction)) {
+			saveRecord(interaction, 10)
+			startRecord(interaction)
+			return interaction.reply({content: 'Saving!', ephemeral: true})
 		}
-		await startRecord(interaction)
-		record.push(interaction.guildId)
-
+		startRecord(interaction)
 		interaction.reply({content: 'Recording started', ephemeral: true})
 	},
 } as Command<SlashCommandBuilder>
